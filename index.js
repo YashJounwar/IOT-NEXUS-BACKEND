dd401035f1e0357401aab37b7a493ec82252ec70
yash jounwar <yash123joun@gmail.comclear>
2024-04-30 15:28:56 +0530

    changes in dashboard

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ export const app = express();
 const server =createServer(app);
 export const io = new Server(server,{
     cors: {
-        origin: "http://localhost:3000"
+        origin: "*"
     }
 });
 
