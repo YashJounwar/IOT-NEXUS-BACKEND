5ee5cb46ecda1372aa3e21f0fb64083e0d0933f6
yash jounwar <yash123joun@gmail.comclear>
2024-04-30 16:40:37 +0530

    changes in dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,9 +3,6 @@ import { auth, refe, db } from "../database/firebase.js";
 import { get, set, update } from "firebase/database";
 import { io } from '../index.js';
 import { mmToInches, volumeOfWater, letersOfWater } from '../utilityFunctions/conversionFns.js'
-io.on('connection',(socket)=>{
-    console.log("client connected with id", socket.id);
-})
 const router = Router();
 let userDeviceInfo = [];
 const allDeviceIDRef = refe(db, '/AllDevicesId');
